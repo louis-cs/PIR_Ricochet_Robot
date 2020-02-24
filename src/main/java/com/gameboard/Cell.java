3d53ecd1f4fdae418e38d80a68303e968de99370
Shoarzi <prieu@etud.insa-toulouse.fr>
2020-02-24 13:34:03 +0100

    Cell and Direction Implemented

---
+++ src/main/java/com/gameboard/Cell.java
@@ -0,0 +1,21 @@
+package com.gameboard;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Cell {
+
+    public Set<Direction> walls ;
+
+    public Cell() {
+        walls = new HashSet<>();
+    }
+
+    public void addWall(Direction d){
+        walls.add(d) ;
+    }
+
+    public boolean PossibleMove(Direction d) {
+        return walls.contains(d);
+    }
+}
---
+++ src/main/java/com/gameboard/Direction.java
@@ -0,0 +1,10 @@
+package com.gameboard;
+
+import com.sun.scenario.effect.Brightpass;
+
+public enum Direction{
+    up ,
+    right ,
+    down ,
+    left;
+}
