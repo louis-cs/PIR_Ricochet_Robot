dfbaa401ea0051cbc31cd94a2628cbe4ce379cf8
Shoarzi <prieu@etud.insa-toulouse.fr>
2020-02-28 14:42:48 +0100

    added Binary Heap

---
+++ src/main/java/com/graph/struct/BinaryHeap.java
@@ -0,0 +1,244 @@
+//
+// ******************PUBLIC OPERATIONS*********************
+// void insert( x ) --> Insert x
+// Comparable deleteMin( )--> Return and remove smallest item
+// Comparable findMin( ) --> Return smallest item
+// boolean isEmpty( ) --> Return true if empty; else false
+// ******************ERRORS********************************
+// Throws RuntimeException for findMin and deleteMin when empty
+
+package com.graph.struct;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+/**
+ * Implements a binary heap. Note that all "matching" is based on the compareTo
+ * method.
+ *
+ * @author Mark Allen Weiss
+ * @author DLB
+ * @author https://github.com/Shoarzi/Raf.git, originally done by Mickael Cappelle.
+ */
+public class BinaryHeap<E extends Comparable<E>> extends PriorityQueue<E> {
+
+    // Number of elements in heap.
+    private int currentSize;
+
+    // The heap array.
+    private final ArrayList<E> array;
+
+    /**
+     * Construct a new empty binary heap.
+     */
+    public BinaryHeap() {
+        this.currentSize = 0;
+        this.array = new ArrayList<E>();
+    }
+
+    /**
+     * Construct a copy of the given heap.
+     *
+     * @param heap Binary heap to copy.
+     */
+    public BinaryHeap(BinaryHeap<E> heap) {
+        this.currentSize = heap.currentSize;
+        this.array = new ArrayList<E>(heap.array);
+    }
+
+    /**
+     * Set an element at the given index.
+     *
+     * @param index Index at which the element should be set.
+     * @param value Element to set.
+     */
+    private void arraySet(int index, E value) {
+        if (index == this.array.size()) {
+            this.array.add(value);
+        }
+        else {
+            this.array.set(index, value);
+        }
+    }
+
+    /**
+     * @return Index of the parent of the given index.
+     */
+    private int index_parent(int index) {
+        return (index - 1) / 2;
+    }
+
+    /**
+     * @return Index of the left child of the given index.
+     */
+    private int index_left(int index) {
+        return index * 2 + 1;
+    }
+
+    /**
+     * Internal method to percolate up in the heap.
+     *
+     * @param index Index at which the percolate begins.
+     */
+    private void percolateUp(int index) {
+        E x = this.array.get(index);
+
+        for (; index > 0
+                && x.compareTo(this.array.get(index_parent(index))) < 0; index = index_parent(
+                index)) {
+            E moving_val = this.array.get(index_parent(index));
+            this.arraySet(index, moving_val);
+        }
+
+        this.arraySet(index, x);
+    }
+
+    /**
+     * Internal method to percolate down in the heap.
+     *
+     * @param index Index at which the percolate begins.
+     */
+    private void percolateDown(int index) {
+        int ileft = index_left(index);
+        int iright = ileft + 1;
+
+        if (ileft < this.currentSize) {
+            E current = this.array.get(index);
+            E left = this.array.get(ileft);
+            boolean hasRight = iright < this.currentSize;
+            E right = (hasRight) ? this.array.get(iright) : null;
+
+            if (!hasRight || left.compareTo(right) < 0) {
+                // Left is smaller
+                if (left.compareTo(current) < 0) {
+                    this.arraySet(index, left);
+                    this.arraySet(ileft, current);
+                    this.percolateDown(ileft);
+                }
+            }
+            else {
+                // Right is smaller
+                if (right.compareTo(current) < 0) {
+                    this.arraySet(index, right);
+                    this.arraySet(iright, current);
+                    this.percolateDown(iright);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.currentSize == 0;
+    }
+
+    @Override
+    public int size() {
+        return this.currentSize;
+    }
+
+    public void insert(E x) {
+        int index = this.currentSize++;
+        this.arraySet(index, x);
+        this.percolateUp(index);
+    }
+
+    //complexite O(n)
+    //a removed object must be in the heap.
+    public void remove(E x) {
+        if (!(this.isEmpty())) {
+            //find x
+            int index = -1  ;
+            index = this.find(x) ;
+            if (!((index==-1) || (index>this.currentSize-1))) {
+                this.arraySet(index, this.array.get(this.currentSize-1)) ;
+                this.currentSize-- ;
+
+                //arnarque moldave
+                this.percolateUp(index);
+                this.percolateDown(index) ;
+            }
+        }
+    }
+
+    public int find(E x) {
+        return this.array.indexOf(x) ;
+    }
+
+    public boolean isValid(int index) {
+        boolean val = true ;
+        boolean val_l = true ;
+        boolean val_r = true ;
+        if (this.index_left(index) < this.size()) {
+            val_l = this.array.get(this.index_left(index)).compareTo(this.array.get(index))>0 && this.isValid(this.index_left(index)) ;
+        }
+        if ((this.index_left(index)+1) < this.size()) {
+            //val = val && this.isValid(this.index_left(index)+1) ;
+            val_r = this.array.get(this.index_left(index)+1).compareTo(this.array.get(index))>0 && this.isValid(this.index_left(index)+1) ;
+        }
+        else {
+            val = (val_r && val_l) ;
+        }
+        return val ;
+    }
+
+    //the heap must have an element to find its min.
+    public E findMin() {
+        return this.array.get(0);
+    }
+
+    public E deleteMin() {
+        E minItem = findMin();
+        E lastItem = this.array.get(--this.currentSize);
+        this.arraySet(0, lastItem);
+        this.percolateDown(0);
+        return minItem;
+    }
+
+    /**
+     * Prints the heap
+     */
+    public void print() {
+        System.out.println();
+        System.out.println("========  HEAP  (size = " + this.currentSize + ")  ========");
+        System.out.println();
+
+        for (int i = 0; i < this.currentSize; i++) {
+            System.out.println(this.array.get(i).toString());
+        }
+
+        System.out.println();
+        System.out.println("--------  End of heap  --------");
+        System.out.println();
+    }
+
+    /**
+     * Prints the elements of the heap according to their respective order.
+     */
+    public void printSorted() {
+
+        BinaryHeap<E> copy = new BinaryHeap<E>(this);
+
+        System.out.println();
+        System.out.println("========  Sorted HEAP  (size = " + this.currentSize + ")  ========");
+        System.out.println();
+
+        while (!copy.isEmpty()) {
+            System.out.println(copy.deleteMin());
+        }
+
+        System.out.println();
+        System.out.println("--------  End of heap  --------");
+        System.out.println();
+    }
+
+    public void testSorted() {
+
+        BinaryHeap<E> copy = new BinaryHeap<E>(this);
+
+        while (!copy.isEmpty()) {
+            System.out.println(copy.deleteMin());
+        }
+    }
+
+}
